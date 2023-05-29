a984bb0c0389a9b8f00014237e1ce331736f7a4e
Bowden Salis <bsalis76@gmail.com>
2023-05-29 15:28:35 +1000

    Added loading overlay to Sysinfo

--- client/web/src/lib/SystemInfo.svelte
+++ client/web/src/lib/SystemInfo.svelte
@@ -6,12 +6,7 @@
   import RubiksCube from '$lib/RubiksCube.svelte';
 
   let looping = true;
-  let info = {
-    version: '', os: '', uptime: 0, cpu: { percent: 0 },
-    memory: { total: 0, used: 0, available: 0, free: 0, percent: 0.0 },
-    disk: { total: 0, used: 0, free: 0, percent: 0.0 },
-    net: { local: '', public: '' }
-  };
+  let info = null;
 
   async function updateSystemInfo() {
     while (looping) {
@@ -42,32 +37,67 @@
   </div>
   <div class="column is-one-third">
     <table class="table is-thinner">
-      <tbody>
-        <tr><td class="has-text-weight-bold">Version</td><td>{info.version}</td></tr>
-        <tr><td class="has-text-weight-bold">OS</td><td>{info.os}</td></tr>
-        <tr><td class="has-text-weight-bold">CPU</td><td>{info.cpu.percent}%</td></tr>
-        <tr><td class="has-text-weight-bold">Memory</td><td></td></tr>
-        <tr><td>Total</td><td>{humanFileSize(info.memory.total)}</td></tr>
-        <tr><td>Used</td><td>{humanFileSize(info.memory.used)}</td></tr>
-        <tr><td>Available</td><td>{humanFileSize(info.memory.available)}</td></tr>
-        <tr><td>Free</td><td>{humanFileSize(info.memory.free)}</td></tr>
-        <tr><td>Usage</td><td>{info.memory.percent}%</td></tr>
-      </tbody>
+      {#if info}
+        <tbody>
+          <tr><td class="has-text-weight-bold">Version</td><td>{info.version}</td></tr>
+          <tr><td class="has-text-weight-bold">OS</td><td>{info.os}</td></tr>
+          <tr><td class="has-text-weight-bold">CPU</td><td>{info.cpu.percent}%</td></tr>
+          <tr><td class="has-text-weight-bold">Memory</td><td></td></tr>
+          <tr><td>Total</td><td>{humanFileSize(info.memory.total)}</td></tr>
+          <tr><td>Used</td><td>{humanFileSize(info.memory.used)}</td></tr>
+          <tr><td>Available</td><td>{humanFileSize(info.memory.available)}</td></tr>
+          <tr><td>Free</td><td>{humanFileSize(info.memory.free)}</td></tr>
+          <tr><td>Usage</td><td>{info.memory.percent}%</td></tr>
+        </tbody>
+      {:else}
+        <tbody class="gray">
+          <tr><td class="has-text-weight-bold">Version</td><td>...</td></tr>
+          <tr><td class="has-text-weight-bold">OS</td><td>...</td></tr>
+          <tr><td class="has-text-weight-bold">CPU</td><td>...</td></tr>
+          <tr><td class="has-text-weight-bold">Memory</td><td>...</td></tr>
+          <tr><td>Total</td><td>...</td></tr>
+          <tr><td>Used</td><td>...</td></tr>
+          <tr><td>Available</td><td>...</td></tr>
+          <tr><td>Free</td><td>...</td></tr>
+          <tr><td>Usage</td><td>...</td></tr>
+        </tbody>
+      {/if}
     </table>
   </div>
   <div class="column is-one-third">
     <table class="table is-thinner">
-      <tbody>
-        <tr><td class="has-text-weight-bold">Uptime</td><td>{humanDuration(info.uptime)}</td></tr>
-        <tr><td class="has-text-weight-bold">Disk</td><td></td></tr>
-        <tr><td>Total</td><td>{humanFileSize(info.disk.total)}</td></tr>
-        <tr><td>Used</td><td>{humanFileSize(info.disk.used)}</td></tr>
-        <tr><td>Available</td><td>{humanFileSize(info.disk.free)}</td></tr>
-        <tr><td>Usage</td><td>{info.disk.percent}%</td></tr>
-        <tr><td class="has-text-weight-bold">IPv4</td><td></td></tr>
-        <tr><td>Local</td><td>{info.net.local}</td></tr>
-        <tr><td>Public</td><td>{info.net.public}</td></tr>
-      </tbody>
+      {#if info}
+        <tbody>
+          <tr><td class="has-text-weight-bold">Uptime</td><td>{humanDuration(info.uptime)}</td></tr>
+          <tr><td class="has-text-weight-bold">Disk</td><td></td></tr>
+          <tr><td>Total</td><td>{humanFileSize(info.disk.total)}</td></tr>
+          <tr><td>Used</td><td>{humanFileSize(info.disk.used)}</td></tr>
+          <tr><td>Available</td><td>{humanFileSize(info.disk.free)}</td></tr>
+          <tr><td>Usage</td><td>{info.disk.percent}%</td></tr>
+          <tr><td class="has-text-weight-bold">IPv4</td><td></td></tr>
+          <tr><td>Local</td><td>{info.net.local}</td></tr>
+          <tr><td>Public</td><td>{info.net.public}</td></tr>
+        </tbody>
+      {:else}
+        <tbody class="gray">
+          <tr><td class="has-text-weight-bold">Uptime</td><td>...</td></tr>
+          <tr><td class="has-text-weight-bold">Disk</td><td>...</td></tr>
+          <tr><td>Total</td><td>...</td></tr>
+          <tr><td>Used</td><td>...</td></tr>
+          <tr><td>Available</td><td>...</td></tr>
+          <tr><td>Usage</td><td>...</td></tr>
+          <tr><td class="has-text-weight-bold">IPv4</td><td>...</td></tr>
+          <tr><td>Local</td><td>...</td></tr>
+          <tr><td>Public</td><td>...</td></tr>
+        </tbody>
+      {/if}
     </table>
   </div>
 </div>
+
+
+<style>
+  .gray {
+    color: #DBDBDB;
+  }
+</style>
