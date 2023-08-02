20f83f2ee64f6ec27d7b49429dfb9b62cdfd413e
Bowden Salis <bsalis76@gmail.com>
2023-08-02 20:22:29 +1000

    Final changes for 0.2.0 release

--- core/util/sysutil.py
+++ core/util/sysutil.py
@@ -17,17 +17,15 @@ class _Cacher:
         return _Cacher.__instance
 
     def __init__(self):
-        self._data = {
-            'os_name': {'task': tasks.task_fork(self._get_os_name(), 'sysutil.os_name'), 'done': False},
-            'local_ip': {'task': tasks.task_fork(self._get_local_ip(), 'sysutil.local_ip'), 'done': False},
-            'public_ip': {'task': tasks.task_fork(self._get_public_ip(), 'sysutil.public_ip'), 'done': False}
-        }
+        self._data = {'os_name': {}, 'local_ip': {}, 'public_ip': {}}
+        self._data['os_name']['task']: asyncio.Task | None = tasks.task_fork(self._get_os_name(), 'get_os_name()')
+        self._data['local_ip']['task']: asyncio.Task | None = tasks.task_fork(self._get_local_ip(), 'get_local_ip()')
+        self._data['public_ip']['task']: asyncio.Task | None = tasks.task_fork(self._get_public_ip(), 'get_public_ip()')
 
     async def get(self, item: str) -> str:
         data = self._data[item]
-        if data['done']:
-            return data['value']
-        await data['task']
+        if data['task']:
+            await data['task']
         return data['value']
 
     async def _get_os_name(self):
@@ -40,10 +38,11 @@ class _Cacher:
                 for line in data.split('\n'):
                     if line.startswith('PRETTY_NAME="'):
                         self._data['os_name']['value'] = line[13:-1]
+                        return
         except Exception as e:
             logging.error('_get_os_name() failed %s', repr(e))
         finally:
-            self._data['os_name']['done'] = True
+            self._data['os_name']['task'] = None
 
     async def _get_local_ip(self):
         self._data['local_ip']['value'] = 'localhost'
@@ -54,19 +53,19 @@ class _Cacher:
         except Exception as e:
             logging.error('_get_local_ip() failed %s', repr(e))
         finally:
-            self._data['local_ip']['done'] = True
+            self._data['local_ip']['task'] = None
 
     async def _get_public_ip(self):
         self._data['public_ip']['value'] = 'UNAVAILABLE'
         try:
-            for url in ('https://api.ipify.org', 'https://ipv4.seeip.org'):
+            for url in ('https://api.ipify.org', 'https://ipv4.seeip.org', 'https://ipinfo.io/ip'):
                 result = await _Cacher._fetch_text(url)
                 if result:
                     self._data['public_ip']['value'] = result
                     logging.debug('Public IP sourced from ' + url)
                     return
         finally:
-            self._data['public_ip']['done'] = True
+            self._data['public_ip']['task'] = None
 
     @staticmethod
     async def _fetch_text(url: str) -> str | None:
@@ -99,23 +98,36 @@ async def get_public_ip() -> str:
 _disk_usage = funcutil.to_async(shutil.disk_usage)
 
 
+def _grep_last_line(startswith: str, text: str | None) -> str | None:
+    if not text:
+        return None
+    result = None
+    for line in text.split('\n'):
+        if line.startswith(startswith):
+            result = line
+    return result
+
+
 async def _virtual_memory() -> tuple:
-    result = await shellutil.run_script('free -b | grep "Mem:"')
+    result = await shellutil.run_executable('free', '-b')
+    result = _grep_last_line('Mem:', result)
     if not result:
         return -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1
     result = result.strip().split(' ')
     result.pop(0)
-    result = [i for i in result if i != '']
-    result = [int(i) for i in result]
+    result = [int(i) for i in result if i]
     result = [result[0], result[5], round(float(result[1] / result[0] * 100), 1),
               result[1], result[2], -1, -1, -1, result[4], -1, -1]
     return tuple(result)
 
 
 async def _cpu_percent() -> float:
-    result = await shellutil.run_script('top -b -n 2 | grep "%Cpu(s)" | tail -1')
+    result = await shellutil.run_executable('top', '-b', '-n', '2')
+    result = _grep_last_line('%Cpu(s)', result)
+    if not result:
+        return 0.0
     result = result.strip().split(' ')
-    result = [i for i in result if i != '']
+    result = [i for i in result if i]
     result = result[result.index('id,') - 1]
     if result == 'ni,100.0':
         return 0.0
