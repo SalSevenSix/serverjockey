334a3c8993c3cfe7b692d95891dad8bd742fcd18
Bowden Salis <bsalis76@gmail.com>
2025-02-27 14:36:44 +0700

    Added support for uploading multiple files

--- client/discord/src/commons.js
+++ client/discord/src/commons.js
@@ -1,21 +1,10 @@
-import * as startup from './commons/startup.js';
-import * as servercmds from './commons/servercmds.js';
-import * as deploymentcmds from './commons/deploymentcmds.js';
-import * as consolecmds from './commons/consolecmds.js';
-import * as aliascmds from './commons/aliascmds.js';
-import * as rewardcmds from './commons/rewardcmds.js';
-import * as activitycmds from './commons/activitycmds.js';
+import { startupAll, startupServerOnly } from './commons/startup.js';
+import { server, auto, log } from './commons/servercmds.js';
+import { getconfig, setconfig, deployment } from './commons/deploymentcmds.js';
+import { send, say, players } from './commons/consolecmds.js';
+import { alias } from './commons/aliascmds.js';
+import { reward } from './commons/rewardcmds.js';
+import { activity } from './commons/activitycmds.js';
 
-export const [
-  startupAll, startupServerOnly,
-  server, auto, log,
-  getconfig, setconfig, deployment,
-  send, say, players,
-  alias, reward, activity
-] = [
-  startup.startupAll, startup.startupServerOnly,
-  servercmds.server, servercmds.auto, servercmds.log,
-  deploymentcmds.getconfig, deploymentcmds.setconfig, deploymentcmds.deployment,
-  consolecmds.send, consolecmds.say, consolecmds.players,
-  aliascmds.alias, rewardcmds.reward, activitycmds.activity
-];
+export { startupAll, startupServerOnly, server, auto, log, getconfig, setconfig, deployment,
+  send, say, players, alias, reward, activity };
--- client/web/src/lib/instance/FileUpload.svelte
+++ client/web/src/lib/instance/FileUpload.svelte
@@ -14,33 +14,45 @@
   export let filenameHelp = 'Please choose a valid filename.';
   export let validateFilename = fTrue;
   export let onCompleted = fNoop;
+  export let multiple = false;
 
   let uploading = false;
-  let uploadFiles = [];
+  let selectedFiles = [];
 
-  $: noFileSelected = uploadFiles.length === 0;
-  $: cannotUpload = uploading || $serverStatus.state === 'MAINTENANCE';
+  $: cannotUpload = selectedFiles.length === 0 || uploading || $serverStatus.state === 'MAINTENANCE';
+  $: displayName = makeDisplayName(selectedFiles); function makeDisplayName(files) {
+    if (files.length === 0) return 'No file selected';
+    if (files.length === 1) return files[0].name;
+    return files[0].name + ' (+' + (files.length - 1) + ' more)';
+  }
 
-  function uploadFile() {
-    if (noFileSelected) return;
-    const filename = uploadFiles[0].name;
-    if (!validateFilename(filename)) return notifyError(filenameHelp);
+  async function uploadFiles() {
+    for (const file of selectedFiles) {
+      const request = newPostRequest(null);
+      request.body = new FormData();
+      request.body.append('file', file);
+      const successful = await fetch(instance.url(rootPath + '/' + file.name), request)
+        .then(function(response) {
+          if (!response.ok) throw new Error('Status: ' + response.status);
+          return notifyInfo(file.name + ' uploaded successfully.', true);
+        })
+        .catch(function() {
+          return notifyError('Failed to upload ' + file.name, false);
+        });
+      if (!successful) return;
+    }
+  }
+
+  function actionUpload() {
+    if (cannotUpload) return;
+    for (const file of selectedFiles) {
+      if (!validateFilename(file.name)) return notifyError(filenameHelp);
+    }
     uploading = true;
-    const request = newPostRequest(null);
-    request.body = new FormData();
-    request.body.append('file', uploadFiles[0]);
-    fetch(instance.url(rootPath + '/' + filename), request)  // Blocks until complete
-      .then(function(response) {
-        if (!response.ok) throw new Error('Status: ' + response.status);
-        notifyInfo(filename + ' uploaded successfully.');
-      })
-      .catch(function() {
-        notifyError('Failed to upload ' + filename);
-      })
-      .finally(function() {
-        uploading = false;
-        onCompleted();
-      });
+    uploadFiles().finally(function() {
+      uploading = false;
+      onCompleted();
+    });
   }
 </script>
 
@@ -55,18 +67,16 @@
 
 <div class="file is-fullwidth is-info has-name">
   <div class="control buttons mb-0 mr-2">
-    <button id="{idPrefix}UploadFile" title="Upload File" class="button is-success"
-            disabled={noFileSelected || cannotUpload} on:click={uploadFile}>
+    <button id="{idPrefix}ActionUpload" title="Upload File" class="button is-success"
+            disabled={cannotUpload} on:click={actionUpload}>
       <i class="fa fa-file-arrow-up fa-lg"></i>&nbsp;&nbsp;Upload</button>
   </div>
   <label class="file-label pr-6">
-    <input id="{idPrefix}Filename" class="file-input" type="file" bind:files={uploadFiles}>
+    <input id="{idPrefix}Filename" class="file-input" type="file" {multiple} bind:files={selectedFiles}>
     <span class="file-cta" title={filenameHelp}>
       <span class="file-icon"><i class="fa fa-file-circle-plus"></i></span>
       <span class="file-label">Choose file…</span>
     </span>
-    <span class="file-name" title={filenameHelp}>
-      {noFileSelected ? 'No file selected' : uploadFiles[0].name}
-    </span>
+    <span class="file-name" title={filenameHelp}>{displayName}</span>
   </label>
 </div>
