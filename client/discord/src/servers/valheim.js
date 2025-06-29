75cf4e089effc9b8593b850929edef87c88a7829
Bowden Salis <bsalis76@gmail.com>
2025-06-29 17:40:47 +0700

    Library upgrades for 0.20 release

--- client/discord/src/servers/valheim.js
+++ client/discord/src/servers/valheim.js
@@ -3,12 +3,12 @@ import * as commons from '../commons.js';
 
 export const startup = commons.startupAll;
 export const { server, auto, log, getconfig, setconfig, deployment, players,
-  alias, reward, trigger, activity, chatlog } = commons;
+  alias, reward, trigger, activity } = commons;
 
 export const help = helptext.newServerHelpBuilder()
   .title('VALHEIM COMMANDS')
   .addServer().addPlayers()
-  .addAlias().addReward().addTrigger().addActivity().addChatlog()
+  .addAlias().addReward().addTrigger().addActivity()
   .addConfig(['cmdargs', 'Adminlist', 'Permittedlist', 'Bannedlist'])
   .addDeployment()
   .build();
