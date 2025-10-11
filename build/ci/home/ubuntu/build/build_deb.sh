926596d5239bbf1b863e3a3f16f1916fe6de1eb4
Bowden Salis <bsalis76@gmail.com>
2025-10-11 16:56:29 +0700

    Added RPM build to CI build pipeline

--- build/ci/home/ubuntu/build/build_deb.sh
+++ build/ci/home/ubuntu/build/build_deb.sh
@@ -5,10 +5,13 @@ echo "Initialising CI build process"
 which wget > /dev/null || exit 1
 which jq > /dev/null || exit 1
 which gh > /dev/null || exit 1
+which docker > /dev/null || exit 1
+
 cd "$(dirname $0)" || exit 1
 BUILD_DIR="$(pwd)"
 BUILD_USER="$(pwd | tr '/' ' ' | awk '{print $2}')"
 DIST_DIR="$BUILD_DIR/dist"
+BUILD_OK_FILE="$DIST_DIR/sjgms/build.ok"
 CI_OK_FILE="$BUILD_DIR/build_deb.ok"
 WEB_DIR="/var/www/downloads"
 [ -d "$WEB_DIR" ] || exit 1
@@ -17,7 +20,31 @@ REPO_URL="https://raw.githubusercontent.com/SalSevenSix/serverjockey/$BRANCH"
 COMMIT_FILE="${BRANCH}-commit.url"
 BRANCH_FILE="/tmp/${$}-${BRANCH}-info.json"
 
+docker image inspect rpmbuilder > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+  echo "CI Build rpmbuilder docker image"
+  rm -rf /tmp/rpmbuilder > /dev/null 2>&1
+  mkdir -p /tmp/rpmbuilder || exit 1
+  cd /tmp/rpmbuilder || exit 1
+  cat <<'EOF' > Dockerfile
+FROM fedora:40
+RUN dnf -y install rpm-build python3-pip
+RUN yum -y install which wget unzip
+RUN python3 -m pip install pipenv
+ARG UID=1000
+ARG GID=1000
+RUN groupadd -g $GID rpmgroup && useradd -u $UID -g rpmgroup -m -s /bin/bash rpmuser
+USER rpmuser
+WORKDIR /home/rpmuser
+RUN curl -fsSL https://bun.sh/install | bash
+ENV PATH="$PATH:/home/rpmuser/.bun/bin"
+ENTRYPOINT ["bash"]
+EOF
+  docker build --build-arg UID=$(id -u $BUILD_USER) --build-arg GID=$(id -g $BUILD_USER) -t rpmbuilder . || exit 1
+fi
+
 echo "CI Checking"
+cd $BUILD_DIR || exit 1
 LAST_URL="none"
 [ -f "$COMMIT_FILE" ] && LAST_URL="$(head -1 $COMMIT_FILE)"
 echo "  last commit     $LAST_URL"
@@ -36,8 +63,6 @@ rm $CI_OK_FILE > /dev/null 2>&1
 echo $CURRENT_URL > $COMMIT_FILE
 chown $BUILD_USER $COMMIT_FILE || exit 1
 chgrp $BUILD_USER $COMMIT_FILE || exit 1
-
-echo "CI Starting"
 systemctl stop serverjockey > /dev/null 2>&1
 rm build.sh > /dev/null 2>&1
 wget -O build.sh $REPO_URL/build/build.sh || exit 1
@@ -45,35 +70,38 @@ chmod 755 build.sh || exit 1
 chown $BUILD_USER build.sh || exit 1
 chgrp $BUILD_USER build.sh || exit 1
 
-echo "CI Building deb"
-cd $BUILD_DIR || exit 1
-su - $BUILD_USER -c "$BUILD_DIR/build.sh $BRANCH"
-BUILD_OK_FILE="$DIST_DIR/sjgms/build.ok"
-[ -f "$BUILD_OK_FILE" ] || exit 1
-TIMESTAMP="$(head -1 $BUILD_OK_FILE)"
+for PKG in rpm deb; do
+  echo "CI Building $PKG"
+  cd $BUILD_DIR || exit 1
+  [ "$PKG" = "deb" ] && su - $BUILD_USER -c "$BUILD_DIR/build.sh $BRANCH"
+  [ "$PKG" = "rpm" ] && docker run -v ${BUILD_DIR}:/home/rpmuser/build rpmbuilder build/build.sh $BRANCH
+  [ -f "$BUILD_OK_FILE" ] || exit 1
+  TIMESTAMP="$(head -1 $BUILD_OK_FILE)"
 
-echo "CI Packaging deb"
-$BUILD_DIR/deb.sh || exit 1
+  echo "CI Packaging $PKG"
+  [ "$PKG" = "deb" ] && $BUILD_DIR/deb.sh || exit 1
+  [ "$PKG" = "rpm" ] && docker run -v ${BUILD_DIR}:/home/rpmuser/build rpmbuilder build/rpm.sh || exit 1
 
-echo "CI Publishing deb"
-cd $DIST_DIR || exit 1
-PKG_FILE="$(ls *.deb | tail -1)"
-TARGET_FILE="sjgms-${BRANCH}-${TIMESTAMP}.deb"
-chown root $PKG_FILE || exit 1
-chgrp root $PKG_FILE || exit 1
-mv $PKG_FILE $WEB_DIR/$TARGET_FILE || exit 1
-cd $WEB_DIR || exit 1
-ln -fs "$TARGET_FILE" "sjgms-${BRANCH}-latest.deb" || exit 1
+  echo "CI Publishing $PKG"
+  cd $DIST_DIR || exit 1
+  PKG_FILE="$(ls *.${PKG} | tail -1)"
+  TARGET_FILE="sjgms-${BRANCH}-${TIMESTAMP}.${PKG}"
+  chown root $PKG_FILE || exit 1
+  chgrp root $PKG_FILE || exit 1
+  mv $PKG_FILE $WEB_DIR/$TARGET_FILE || exit 1
+  cd $WEB_DIR || exit 1
+  ln -fs "$TARGET_FILE" "sjgms-${BRANCH}-latest.${PKG}" || exit 1
 
-echo "CI Cleanup deb"
-rm -rf "$DIST_DIR" > /dev/null 2>&1
-KEEP_COUNT=6
-ls -t sjgms-${BRANCH}-*.deb | while read file; do
-  [ $KEEP_COUNT -gt 0 ] || rm $file
-  ((KEEP_COUNT = KEEP_COUNT - 1))
+  echo "CI Cleanup $PKG"
+  rm -rf "$DIST_DIR" > /dev/null 2>&1
+  KEEP_COUNT=6
+  ls -t sjgms-${BRANCH}-*.${PKG} | while read file; do
+    [ $KEEP_COUNT -gt 0 ] || rm $file
+    ((KEEP_COUNT = KEEP_COUNT - 1))
+  done
 done
 
-echo "CI Docker"
+echo "CI Docker deb"
 cd $BUILD_DIR || exit 1
 rm -rf docker > /dev/null 2>&1
 mkdir docker || exit 1
@@ -85,7 +113,7 @@ wget -O build.sh $REPO_URL/build/docker/build.sh || exit 1
 chmod 755 build.sh || exit 1
 ./build.sh $BRANCH || exit 1
 
-echo "CI Upgrade"
+echo "CI Upgrade deb"
 cd $WEB_DIR || exit 1
 apt -y remove sjgms || exit 1
 apt -y install ./$TARGET_FILE || exit 1
@@ -94,6 +122,7 @@ echo "CI Finishing"
 cd $BUILD_DIR || exit 1
 echo $TIMESTAMP > $CI_OK_FILE
 /usr/local/bin/serverjockey_cmd.pyz -t wait:20 -c emailtoken
+docker system prune > /dev/null 2>&1
 
 echo "CI Done build process"
 exit 0
--- build/rpm.sh
+++ build/rpm.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "Initialising RPM packaging"
-[ "$(whoami)" = "root" ] || exit 1
 cd "$(dirname $0)/dist" || exit 1
 DIST_DIR="$(pwd)"
 RPMBUILD_DIR="$DIST_DIR/rpmbuild"
@@ -14,12 +13,13 @@ TAR_DIR="${TARGET_DIR}-${VERSION}"
 rm -rf $RPMBUILD_DIR $TAR_DIR $TARGET_DIR/DEBIAN > /dev/null 2>&1
 
 echo "Building rpmbuild directory"
-[ -d "$RPMBUILD_DIR/BUILD" ] || mkdir -p $RPMBUILD_DIR/BUILD || exit 1
-[ -d "$RPMBUILD_DIR/BUILDROOT" ] || mkdir -p $RPMBUILD_DIR/BUILDROOT || exit 1
-[ -d "$RPMBUILD_DIR/RPMS" ] || mkdir -p $RPMBUILD_DIR/RPMS || exit 1
-[ -d "$RPMBUILD_DIR/SOURCES" ] || mkdir -p $RPMBUILD_DIR/SOURCES || exit 1
-[ -d "$RPMBUILD_DIR/SPECS" ] || mkdir -p $RPMBUILD_DIR/SPECS || exit 1
-[ -d "$RPMBUILD_DIR/SRPMS" ] || mkdir -p $RPMBUILD_DIR/SRPMS || exit 1
+mkdir $RPMBUILD_DIR || exit 1
+mkdir $RPMBUILD_DIR/BUILD || exit 1
+mkdir $RPMBUILD_DIR/BUILDROOT || exit 1
+mkdir $RPMBUILD_DIR/RPMS || exit 1
+mkdir $RPMBUILD_DIR/SOURCES || exit 1
+mkdir $RPMBUILD_DIR/SPECS || exit 1
+mkdir $RPMBUILD_DIR/SRPMS || exit 1
 cp "$TARGET_DIR/SPECS/sjgms.spec" "$RPMBUILD_DIR/SPECS/sjgms.spec" || exit 1
 
 echo "Preparing files"
@@ -34,7 +34,6 @@ echo "Building RPM file"
 tar --create --file "sjgms-${VERSION}.tar.gz" "sjgms-${VERSION}" || exit 1
 mv $DIST_DIR/sjgms-$VERSION.tar.gz $RPMBUILD_DIR/SOURCES || exit 1
 rpmbuild --define "_topdir $RPMBUILD_DIR" -bb $RPMBUILD_DIR/SPECS/sjgms.spec || exit 1
-[ -f "$RPM_FILE" ] || exit 1
 mv $RPM_FILE $DIST_DIR || exit 1
 
 echo "Cleanup"
