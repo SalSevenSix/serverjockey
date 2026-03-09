a11e028b8de392592c5ef52404f663a2d8a4e42d
Bowden Salis <bsalis76@gmail.com>
2026-03-09 14:07:30 +0700

    Final changes for 0.29 release

--- client/discord/src/servers/testserver.js
+++ client/discord/src/servers/testserver.js
@@ -2,12 +2,12 @@ import * as helptext from '../system/helptext.js';
 import * as commons from '../system/commons.js';
 
 export const startup = commons.startupAll;
-export const { status, server, auto, log, getconfig, setconfig, deployment, players, send, chat,
+export const { status, server, auto, log, getconfig, setconfig, deployment, players, send, say, chat,
   channel, aliasme, alias, reward, trigger, activity, chatlog } = commons;
 
 export const help = helptext.newServerHelpBuilder()
   .title('TEST SERVER COMMANDS')
-  .addServer(true, true).addPlayers().addChat().addSend().addChannel()
+  .addServer(true, true).addPlayers().addSay().addChat().addSend().addChannel()
   .addAliasme().addAlias().addReward().addTrigger().addActivity().addChatlog()
   .addConfig(['cmdargs']).addDeployment(true)
   .build();
