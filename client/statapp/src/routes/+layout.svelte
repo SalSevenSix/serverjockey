8556175da2377d0adb7e14e6947e6b462c742112
Bowden Salis <bsalis76@gmail.com>
2026-08-20 16:57:55 +0700

    Added ability to force mobile view in Statapp

--- client/statapp/src/routes/+layout.svelte
+++ client/statapp/src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script>
   import './styles.css';
+  import { onMount } from 'svelte';
+
+  onMount(async function() {
+    const compact = new URLSearchParams(window.location.search).get('compact');
+    if (compact || compact === '') {
+      await import('./compact.css');
+    }
+  });
 </script>
 
 
